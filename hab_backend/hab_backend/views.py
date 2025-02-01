5b92fa4e2029779c3aa76556054d82b042758609
Eric <wang.eric027@gmail.com>
2025-02-01 17:06:10 -0500

    COnfigured backend

--- hab_backend/hab_backend/views.py
+++ hab_backend/hab_backend/views.py
@@ -1,2 +1,15 @@
-def login_redirect():
-    return 0
+from django.http import JsonResponse
+from django.http import HttpResponse
+from rest_framework.decorators import api_view
+
+username = "Eric"
+password = "Pass"
+
+@api_view(['POST'])
+def login_redirect(request):
+    username_input = request.POST.get('username')
+    password_input = request.POST.get('password')
+    if username == username_input and password == password_input:
+        print("Successfully logged in")
+        return JsonResponse({}, status=200)
+
